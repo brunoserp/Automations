580e2f6c55eed4c34772c7857326b10651f8847d
Bruno <107195623+brunoserp@users.noreply.github.com>
2023-09-17 14:30:00 -0300

    Update and rename extrair numero chave nf de xml.py to extrair a chave NFe (44 dígitos) do xml.py

--- extrair a chave NFe (44 dígitos) do xml.py
+++ extrair a chave NFe (44 dígitos) do xml.py
@@ -1,3 +1,20 @@
+'''
+Esse programa não está dando o resultado esperado.
+
+Era esperado que ele fizesse o csv com cada chave da NF (de 44 dígitos) numa linha. Porém não consegui fazer assim.
+Basicamente, eu fiz um programa que, iterando sobre cada XMLs de uma pasta, extrai a chave da NF e a adiciona a uma lista.
+Ao final do programa ele adiciona essa lista a um csv.
+
+Porém, das formas que tentei, ele faz o csv com:
+-> do jeito que fiz abaixo, ele cria o csv com cada chave por linha, porém, com uma vírgula entre cada um dos 44 dígitos
+-> se eu substituísse writerow por writerows, ele colocaria cada dígito numa linha do csv
+-> se eu tirasse o for e fizesse writerow(lista_xmls), ele colocaria toda a lista na mesma célula do Excel. Se eu fizesse por writerows(lista_xmls) ele faria do mesmo jeito que esse programa abaixo.
+
+Suspeito que o problema esteja na hora de salvar. 
+Quando eu printo lista_xmls aparece a lista com cada chave num elemento, conforme esperado
+Já mexi no newline, delimeter e converti cada chave pra string e em nenhum tive o resultado desejado.
+'''
+
 # Caso vc queira entender o código, fiz um explicativo no final
 # Aperte F5 e preste atenção no terminal (dê um enter pro Python processar a informação que vc inseriu)
 
@@ -18,8 +35,8 @@ for filename in os.listdir(pasta):                                      # pra ca
         for item in root.iter():                                        # pra cada item dentro das raízes:
             if item.tag == '{http://www.portalfiscal.inf.br/nfe}chNFe': # se a tag for essa
                 lista_xmls.append(item.text)                            # adiciona o elemento dessa tag na lista_xmls
-print(lista_xmls)
 
+# Salvar a lista no csv:
 pasta_csv = pasta + '/lista_xmls.csv'
 with open(pasta_csv, 'w', newline='') as f:                             # newline = '' pra evitar do python criar o CSV pulando uma linha entre os valores
     write = csv.writer(f, delimiter = ',')
