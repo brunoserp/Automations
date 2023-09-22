b48d6897e4a9c49f6b5efce960c6d9cad69bd924
Bruno <107195623+brunoserp@users.noreply.github.com>
2023-09-21 22:09:16 -0300

    Update conferir XML solicitado x enviado.py

--- conferir XML solicitado x enviado.py
+++ conferir XML solicitado x enviado.py
@@ -18,5 +18,5 @@ df_solicitados = pd.DataFrame(csv_solicitados)
 verificacao = df_solicitados.isin(df_enviados)
 df_solicitados["Enviados?"] = verificacao
 
-df_solicitados.to_csv(f"{xmls_solicitados[:-4]}_python{xmls_solicitados[-4:]}")
+df_solicitados.to_csv(f"{xmls_solicitados[:-4]}_python{xmls_solicitados[-4:]}",index=False)
 input("Pronto. O arquivo com os XMLs verificados foi salvo na mesma pasta do arquivo com os XMLs solicitados, porém com um '_python' no nome. Ele tá com a 1ª coluna com index (0,1,2,etc), pode excluir se quiser.")
